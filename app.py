3f4913f313741d46eed7a2e1864f23adc53e5432
Mubashir <cs.mubashir.a@gmail.com>
2024-09-14 17:46:52 +0500

    Create an api

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from data_cleaning import clean
 from model_building import model
 from model_io import model_save , model_load
+from fastapi import FastAPI
+
 
 file_path = 'D:\AI\ML\Cohort\MLOPs\Portfolio\Student_Performance.csv'
 
@@ -8,4 +10,11 @@ cleaned_data = clean(file_path)
 trained_model = model(cleaned_data)
 saved_model = model_save(trained_model)
 load_model = model_load(saved_model)
+
+app = FastAPI()
+
+@app.get('/')
+async def home():
+    return "Predict the Student Performance with Mubashir Aziz"
+
 
