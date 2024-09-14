296a51a1d8793904d30f583e910bb215b6a38dfb
Mubashir <cs.mubashir.a@gmail.com>
2024-09-14 17:49:59 +0500

    Modified

--- model_building.py
+++ model_building.py
@@ -17,8 +17,4 @@ def model(df):
     mse = mean_squared_error(y_test, y_pred)
     r2 = r2_score(y_test, y_pred)
 
-    print(f'Mean Absolute Error: {mae}')
-    print(f'Mean Squared Error: {mse}')
-    print(f'R-squared: {r2}')
-
     return reg
